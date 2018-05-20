3d14381efc595b61f7e267dea108da96b3a6d771
Timo Goebel <mail@timogoebel.name>
2018-05-20 14:11:36 +0200

    fixes #22388 - jwt token auth

---
+++ app/models/jwt_secret.rb
@@ -0,0 +1,23 @@
+class JwtSecret < ApplicationRecord
+  include Encryptable
+
+  encrypts :token
+
+  belongs_to :user, inverse_of: :jwt_secret
+
+  validates :token, uniqueness: true
+  validates :user, presence: true
+
+  before_save :generate_token, on: :create, prepend: true, :unless => Proc.new {|j| j.token.present?}
+
+  scope :for_user, ->(user) { find_by(user: user) }
+
+  private
+
+  def generate_token
+    loop do
+      self.token = SecureRandom.base64
+      break unless JwtSecret.find_by(token: token)
+    end
+  end
+end
