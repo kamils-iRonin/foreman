76e2f41ddfa886351f5bc91a0d5836174f087448
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2020-02-19 12:17:42 +0100

    Fixes 29027 - fix AlterSessionSequenceToCycle migration

--- db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
+++ db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
@@ -3,7 +3,7 @@ class AlterSessionSequenceToCycle < ActiveRecord::Migration[5.2]
     change_column :sessions, :id, :bigint
     if ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
       ActiveRecord::Base.connection.execute(<<-SQL)
-        ALTER SEQUENCE sessions_id_seq MAXVALUE 9223372036854775807 CYCLE;
+        ALTER SEQUENCE sessions_id_seq AS bigint MAXVALUE 9223372036854775807 CYCLE;
       SQL
     end
   end
